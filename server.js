74d2b6eec838549a63df511fe0eb4e67ed020c7a
Max <Dis0x.1@gmail.com>
2013-03-09 18:41:01 +0100

    end of thenodebeginner tutorial

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-var http = require("http");
-var url = require("url");
-var port = process.env.PORT || 3000;
+var http = require("http"),
+    url = require("url"),
+    port = process.env.PORT || 3000;
 
 function start(route, handle) {
   http.createServer(function(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
-
-    route(handle, pathname, response);
+    route(handle, pathname, response, request);
   }).listen(port);
   console.log("Server has started.");
 }
